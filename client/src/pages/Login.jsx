3233bc7b14eb8ac01df801ac9cdc7ff32be9faf2
teez-ai <songcokurt.ad@gmail.com>
2026-05-16 20:59:41 -0700

    Finish styling for Login and Registration Forms

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,1 +1,11 @@
-// TODO: Login page
+import LoginForm from "../components/auth/LoginForm";
+
+function Login() {
+  return (
+    <main className="auth-page">
+      <LoginForm />
+    </main>
+  );
+}
+
+export default Login;

76f7001a5c01f9a9452e94261fd6290e867a3be3
mozunagarcia <ozunamariluz@gmail.com>
2026-05-21 23:35:54 -0700

    Designed mongoose schemas

--- server/src/models/Group.js
+++ server/src/models/Group.js
@@ -1,1 +1,30 @@
-// TODO: Group mongoose model
+const mongoose = require("mongoose");
+
+const groupSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    maxSize: { type: Number, default: 6, min: 2, max: 20 },
+    description: { type: String, maxlength: 500 },
+    meetingSchedule: { type: String },
+    sharedLinks: [{ type: String, trim: true }],
+    status: {
+      type: String,
+      enum: ["forming", "active", "completed"],
+      default: "forming",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Group", groupSchema);
--- server/src/models/Rating.js
+++ server/src/models/Rating.js
@@ -1,1 +1,33 @@
-// TODO: Rating mongoose model
+const mongoose = require("mongoose");
+
+const ratingSchema = new mongoose.Schema(
+  {
+    rater: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    ratee: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    group: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Group",
+      required: true,
+    },
+    score: { type: Number, required: true, min: 1, max: 5 },
+    categories: {
+      reliability: { type: Number, min: 1, max: 5 },
+      communication: { type: Number, min: 1, max: 5 },
+      contribution: { type: Number, min: 1, max: 5 },
+    },
+    comment: { type: String, maxlength: 500 },
+  },
+  { timestamps: true }
+);
+
+ratingSchema.index({ rater: 1, ratee: 1, group: 1 }, { unique: true });
+
+module.exports = mongoose.model("Rating", ratingSchema);
